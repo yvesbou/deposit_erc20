5421cc57709c310a9e426bba0ce201af4ca0fafb
Yves Boutellier <63062257+yvesbou@users.noreply.github.com>
2022-06-21 07:40:15 +0200

    Feat/deposit (#2)
    * deposit function written
    
    * added mock contract for usdc
    
    * added tests, adapted contract code
    
    * changed to custom errors instead of require

--- contract/contracts/Fund.sol
+++ contract/contracts/Fund.sol
@@ -1,11 +1,75 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 
 contract Fund {
+    mapping(address => uint256) public addressToFunds;
+    mapping(address => bool) public addressToFundingStatus;
+    address[] public funders;
 
-    constructor() {
+    // USDC token parameters.
+    address public usdcAddress;
+    IERC20 public usdcContract;
 
+    event Deposit(uint256 amount, address indexed sender);
+    event NewFunder(address indexed funder);
+
+     /// @notice Initialise the contract
+     /// @param _usdcAddress address of the USDC Token Contract   
+    constructor(address _usdcAddress) {
+        usdcAddress = _usdcAddress;
+        usdcContract = IERC20(usdcAddress);
     }
+
+    /// @notice Deposit USDC to the Fund Contract.
+    /// @param _amount The Amount of USDC token send to the Fund.
+    function deposit(uint256 _amount) external {
+
+        if (_amount <= 0) {
+            revert InsufficientAmount({depositAmount: _amount});
+        }
+        uint256 userBalance = usdcContract.balanceOf(msg.sender);
+        if (userBalance < _amount) {
+            revert InsufficientBalance({available: userBalance, required: _amount});
+        }
+        if (!usdcContract.transferFrom(msg.sender, address(this), _amount)){
+            revert TransactionDeclined();
+        }
+
+        addressToFunds[msg.sender] += _amount;
+        emit Deposit(_amount, msg.sender);
+
+        if (! addressToFundingStatus[msg.sender]) {
+            funders.push(msg.sender);
+            addressToFundingStatus[msg.sender] = true;
+            emit NewFunder(msg.sender);
+        }
+    }
+
+    /***********************************************************************************************
+                                            Custom errors
+    ***********************************************************************************************/
+
+    /// Insufficient balance for transfer. Needed `required` but only
+    /// `available` available.
+    /// @param available balance available.
+    /// @param required requested amount to transfer.
+    error InsufficientBalance(uint256 available, uint256 required);
+
+    /// User wanted to deposit insufficient amount
+    /// @param depositAmount amount tried to deposit
+    error InsufficientAmount(uint256 depositAmount);
+
+    /// User declined transaction
+    error TransactionDeclined();
+
+}
+
+// Interfaces
+
+interface IERC20 {
+    function transfer(address receiverAddress, uint amount) external returns (bool);
+    function transferFrom(address senderAddress, address receiverAddress, uint amount) external returns (bool);
+    function balanceOf(address userAddress) external view returns (uint);
 }
--- contract/test/fund.js
+++ contract/test/fund.js
@@ -1,12 +1,84 @@
+const hre = require("hardhat");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Deploy", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Fund = await ethers.getContractFactory("Fund");
-    const fund = await Fund.deploy();
+describe("Fund Contract Tests", () => {
+  let USDCContract, fund, owner, user;
+
+  beforeEach(async () => {
+    
+    // setting up testing accounts
+    [owner, user] = await ethers.getSigners();
+    let usdcTreasuryAmount = ethers.utils.parseEther("100")
+
+    // setup USDC contract
+    const USDCTokenFactory = await ethers.getContractFactory("USDCToken");
+    USDCContract = await USDCTokenFactory.deploy(usdcTreasuryAmount); // 100 USDC
+    await USDCContract.deployed();
+
+    // setup the Fund contract
+    const FundTokenFactory = await ethers.getContractFactory("Fund");
+    fund = await FundTokenFactory.deploy(USDCContract.address);
     await fund.deployed();
+  })
+
+  describe("Deposit", () => {
+
+    it("The User should be allowed to deposit available USDC", async () => {
+
+      // setup user USDC balance for interaction with contract
+      let balance = await USDCContract.balanceOf(user.address);
+      expect(balance).to.equal(0);
+      
+      let txn = await USDCContract.transfer(user.address, ethers.utils.parseEther("5")); // user1 received 5 USDC
+      await txn.wait();
+      
+      balance = await USDCContract.balanceOf(user.address);
+      expect(balance).to.equal(ethers.utils.parseEther("5"));
+      
+      // User deposits 5 USDC (first approval, then deposit to contract)
+      txn = await USDCContract.connect(user).approve(fund.address, ethers.utils.parseEther("5"));
+      await txn.wait();
+      
+      txn = await fund.connect(user).deposit(ethers.utils.parseEther("5"));
+      await txn.wait();
+      
+      // funders and addressToFunds are the default getters solidity is generatings for us
+      let amountFunded = await fund.addressToFunds(user.address);
+      let funderAddress = await fund.funders(0); 
+
+      // user is now part of the funders
+      expect(funderAddress).to.equal(user.address);
+      
+      // user has deposited 5 USDC
+      expect(amountFunded).to.equal(ethers.utils.parseEther("5"));
+      
+    });
+
     
-    
-  });
+    it("The amount to deposit should be larger than 0", async () => {
+
+      txn = await USDCContract.connect(user).approve(fund.address, ethers.utils.parseEther("5"));
+      await txn.wait();
+      let amountToBeDeposited = ethers.utils.parseEther("0");
+      await expect(fund.connect(user).deposit(amountToBeDeposited)).to.be.revertedWith(`InsufficientAmount(${amountToBeDeposited})`);
+
+    })
+
+    it("The USDC balance of user should be greater than amount to be deposited", async () => {
+      // user balance is 0
+      let userBalance = await USDCContract.balanceOf(user.address);
+      expect(userBalance).to.equal(0);
+
+      let amount = ethers.utils.parseEther("5");
+
+      // User approves 5 USDC (first approval, then deposit to contract)
+      txn = await USDCContract.connect(user).approve(fund.address, amount);
+      await txn.wait();
+
+      await expect(fund.connect(user).deposit(amount)).to.be.revertedWith(`InsufficientBalance(${userBalance}, ${amount})`);
+
+    })
+
+  }) 
 });
