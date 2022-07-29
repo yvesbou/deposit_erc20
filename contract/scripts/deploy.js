b876ae422469a8cce1a0cfbd6d483d6a5601afb2
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-07-29 08:11:23 +0200

    add verification of contract to deploy script

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -1,4 +1,3 @@
-// const { ethers } = require("hardhat");
 const hre = require("hardhat")
 const { fundConfig } = require("../helper.config");
 
@@ -7,20 +6,17 @@ async function main() {
     console.log(hre.network.config.chainId)
 
 	if (hre.network.config.chainId == 31337) {
-		// deploying on localhost so we deploy the mock contracts.
-
-		// Deploy the MockUsdcToken contract.
+		// Deploying on localhost...
+		// Deploy the Mock USDCStablecoin contract
 		const usdcTokenContractFactory = await hre.ethers.getContractFactory("USDCStablecoin");
-        // const totalSupplyUSDC = ethers.utils.parseEther("10000");
 		usdcTokenContract = await usdcTokenContractFactory.deploy();
-        console.log('here')
 		await usdcTokenContract.deployed();
 		console.log("MockUSDCStablecoin contract deployed to:", usdcTokenContract.address);
 		usdcTokenAddress = usdcTokenContract.address;
 	} else {
 		// Retrieve network-specific parameters.
 		const chainId = hre.network.config.chainId;
-        consolge.log(chainId);
+        console.log(chainId);
 		usdcTokenAddress = fundConfig.network[chainId].usdcTokenAddress;
 	}
 
@@ -31,9 +27,13 @@ async function main() {
 
 	console.log("Fund deployed to:", fund.address);
 
-    // Verify the contract.
-	console.log(`Verify with: $ npx hardhat verify ${fund.address} ${usdcTokenAddress} --contract contracts/Fund.sol:Fund --network ${hre.network.name}`);
-
+    // Verify the contract
+    if (hre.network.config.chainId == 31337) {
+        await fund.deployTransaction.wait(6);
+        await hre.run("verify:verify", {
+            address: fund.address,
+          });
+    }
 }
 
 main()
